fe829a7392e5b57034b18e2daf788b69cd5bc26b
Hawraa7 <hawraahijazi1996@gmail.com>
2025-06-03 10:34:35 +0200

    Creating the page of product_detail.html and installing pillow

--- products/urls.py
+++ products/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
-    path('<str:category_name>/', views.products_by_category, name='products_by_category'),
+    path('<int:product_id>/', views.product_detail, name='product_detail'),
 ]
+
--- products/views.py
+++ products/views.py
@@ -2,17 +2,22 @@ from django.shortcuts import render, get_object_or_404
 from .models import Product, Category
 
 def all_products(request):
+    """ View to show all products """
     products = Product.objects.all()
+    category = request.GET.get('category', None)
+
+    if category:
+        products = products.filter(category__name=category)
+
     context = {
         'products': products,
+        'current_category': category,
     }
     return render(request, 'products/products.html', context)
 
-def products_by_category(request, category_name):
-    category = get_object_or_404(Category, name=category_name)
-    products = Product.objects.filter(category=category)
-    context = {
-        'category': category,
-        'products': products,
-    }
-    return render(request, 'products/products_by_category.html', context)
+
+def product_detail(request, product_id):
+    """ View to show individual product details """
+    product = get_object_or_404(Product, pk=product_id)
+    return render(request, 'products/product_detail.html', {'product': product})
+
