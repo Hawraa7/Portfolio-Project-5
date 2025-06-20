9483c085fc8e7b4a538870f48d57422ad4d5bdf1
Hawraa7 <hawraahijazi1996@gmail.com>
2025-06-20 16:35:38 +0200

    Set debug to false for deploy

--- fitness_portal/settings.py
+++ fitness_portal/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3mr!7zs@ye0t&(1oaj12@4!+6fi!r(4aelwdmb1&elf7u)-au7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
