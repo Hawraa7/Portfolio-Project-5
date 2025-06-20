458e309a0e41d3ce2f9b4ce0c23afa97e49ba934
Hawraa7 <hawraahijazi1996@gmail.com>
2025-06-20 15:41:14 +0200

    Preparing the code to deploy

--- fitness_portal/settings.py
+++ fitness_portal/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 import os 
+import dj_database_url
 from pathlib import Path
 
 if os.path.isfile('env.py'):
@@ -31,6 +32,7 @@ DEBUG = True
 ALLOWED_HOSTS = [
     '127.0.0.1',
     'localhost',
+    'project-portfolio5-82d29c509b78.herokuapp.com',
 ]
 
 
@@ -123,12 +125,17 @@ WSGI_APPLICATION = 'fitness_portal.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 
@@ -185,5 +192,7 @@ DEFAULT_FROM_EMAIL = 'zouzoufitness@example.com'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
