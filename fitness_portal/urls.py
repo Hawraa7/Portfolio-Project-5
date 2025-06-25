965cf8bd5fa711f8f1525ba73f9f2f17f74ccb8e
Hawraa7 <hawraahijazi1996@gmail.com>
2025-06-25 09:37:08 +0200

    Create the 404 page

--- fitness_portal/urls.py
+++ fitness_portal/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 
 
@@ -31,3 +32,5 @@ urlpatterns = [
     path('classes/', include('classes.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'fitness_portal.views.handler404'
