f82d57b4d4a38d3ae419d7f8614ca765da079352
Hawraa7 <hawraahijazi1996@gmail.com>
2025-07-01 02:18:43 +0200

    debug true

--- fitness_portal/settings.py
+++ fitness_portal/settings.py
@@ -27,7 +27,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 #SECRET_KEY = 'django-insecure-3mr!7zs@ye0t&(1oaj12@4!+6fi!r(4aelwdmb1&elf7u)-au7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+#DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
 
 ALLOWED_HOSTS = [
