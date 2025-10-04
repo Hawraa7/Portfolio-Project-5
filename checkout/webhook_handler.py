3bc6bc9b44c3655d473c553b3d91c45470333670
Hawraa7 <hawraahijazi1996@gmail.com>
2025-10-04 21:27:21 +0200

    last testing with emails

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -46,22 +46,22 @@ class StripeWH_Handler:
         try:
             intent = event.data.object
             pid = intent.id
-            send_mail(
-                'Test 4 from Zouzou’s Fitness',
-                'If you’re reading this, Gmail App Passwords work 🎉',
-                settings.DEFAULT_FROM_EMAIL,
-                ['hawraahijazi1996@gmail.com']
-            )
             bag = intent.metadata.bag
             save_info = intent.metadata.save_info
+            billing_details = intent.charges.data[0].billing_details
             send_mail(
                 'Test 3 from Zouzou’s Fitness',
                 'If you’re reading this, Gmail App Passwords work 🎉',
                 settings.DEFAULT_FROM_EMAIL,
                 ['hawraahijazi1996@gmail.com']
             )
-            billing_details = intent.charges.data[0].billing_details
             shipping_details = intent.shipping
+            send_mail(
+                'Test 4 from Zouzou’s Fitness',
+                'If you’re reading this, Gmail App Passwords work 🎉',
+                settings.DEFAULT_FROM_EMAIL,
+                ['hawraahijazi1996@gmail.com']
+            )
             grand_total = round(intent.charges.data[0].amount / 100, 2)
             send_mail(
                 'Test 2 from Zouzou’s Fitness',
