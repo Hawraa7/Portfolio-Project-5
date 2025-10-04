62b2005e1e69d6158093ff5963817401cccb6b54
Hawraa7 <hawraahijazi1996@gmail.com>
2025-10-05 01:35:36 +0200

    final test for sending confirmation email while updating the stripe element

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -61,6 +61,7 @@ form.addEventListener('submit', function(ev) {
         'csrfmiddlewaretoken': csrfToken,
         'client_secret': clientSecret,
         'save_info': saveInfo,
+        'email': $.trim(form.email.value),
     };
     var url = '/checkout/cache_checkout_data/';
 
